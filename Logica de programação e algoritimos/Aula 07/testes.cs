56fc3923081323b19d2acb734891e96c64597178
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-02-27 20:54:34 -0300

    sexta feira

---
+++ Logica de programação e algoritimos/Aula 07/testes.cs
@@ -0,0 +1,9 @@
+static double soma(double a = 0, double b = 0)
+{
+    Console.WriteLine("Teste");
+    return a + b;
+}
+
+System.Console.WriteLine(soma(1, 2));
+System.Console.WriteLine(soma(1));
+System.Console.WriteLine(soma());
