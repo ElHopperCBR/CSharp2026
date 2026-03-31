4d07b12610f3bf98bf98568b698913cd68e66259
ProfPlatini <platiniluiz@outlook.com>
2026-03-30 21:55:58 -0300

    aula 30.03

--- POO/Aula 07/Heranca/Heranca/obj/Debug/net10.0/Heranca.AssemblyInfo.cs
+++ POO/Aula 07/Heranca/Heranca/obj/Debug/net10.0/Heranca.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Banco1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e919ca37d349748b1f2bfd28687fb4d2906e7110")]
-[assembly: System.Reflection.AssemblyProductAttribute("Banco1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Banco1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Heranca")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
