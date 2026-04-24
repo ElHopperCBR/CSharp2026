d7daaa755d1338862b200257d3475a260bb09513
ProfPlatini <platiniluiz@outlook.com>
2026-04-24 18:31:20 -0300

    Atualizadp

--- Banco de Dados/Aula 02/Estudante/Estudante/Classes/Entidades/Aluno.cs
+++ Banco de Dados/Aula 02/Estudante/Estudante/Classes/Entidades/Aluno.cs
@@ -28,6 +28,6 @@
             Console.WriteLine($"Curso matriculado: {Curso}");
         }
 
- 
+
     }
 }
--- Banco de Dados/Aula 02/Estudante/Estudante/obj/Debug/net10.0/Estudante.AssemblyInfo.cs
+++ Banco de Dados/Aula 02/Estudante/Estudante/obj/Debug/net10.0/Estudante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estudante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997f7eb45c8f2fa732f126da241a0bbee3fd0579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9ed08f8f2bb16163b7f6ee886d4f6064626b804")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estudante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estudante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
