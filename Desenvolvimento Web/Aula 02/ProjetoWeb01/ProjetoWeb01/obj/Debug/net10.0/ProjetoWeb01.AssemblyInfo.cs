df898abae58668c4741dcc202f7d3cc32bacf3b3
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-05-27 20:52:18 -0300

    pablo

--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c1f5b403baa7c1e6c04b23edbb67b3c9a196571")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a91f84ca6f8732b2c1ddf53db3a518eb0593cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
