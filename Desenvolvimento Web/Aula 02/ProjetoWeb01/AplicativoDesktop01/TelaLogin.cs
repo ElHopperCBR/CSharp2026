b4de1ceb7d8387f27b9774c49af173c63e8dfc19
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-06-03 21:32:29 -0300

    Feito aplicativo desktop front end

---
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/AplicativoDesktop01/TelaLogin.cs
@@ -0,0 +1,31 @@
+namespace AplicativoDesktop01
+{
+    public partial class TelaLogin : Form
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = "Ronaldo";
+            string senha = "123456";
+
+            bool comparacao1 = textBox1.Text == usuario;
+            bool comparacao2 = textBox2.Text == senha;
+
+            if (comparacao1 & comparacao2)
+            {
+                MessageBox.Show("Usuario e senha corretos!");
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Usuario ou senha incorretos");
+            }
+        }
+
+    }
+}
