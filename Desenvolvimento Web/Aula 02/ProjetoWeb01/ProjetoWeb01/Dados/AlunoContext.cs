39b9562a9c11d37cf45056f48ba0d8fdfe04abc6
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-06-10 21:33:59 -0300

    Finalizado projeto WEB

--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/Dados/AlunoContext.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/Dados/AlunoContext.cs
@@ -6,7 +6,7 @@ namespace ProjetoWeb01.Dados
     public class AlunoContext : DbContext
     {
 
-        //public AlunoContext(DbContextOptions<AlunoContext> options) :base(options) { }
+       // public AlunoContext(DbContextOptions<AlunoContext> options) :base(options) { }
 
         public DbSet<Aluno> Alunos { get; set; }
 
--- Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb01/ProjetoWeb01/obj/Debug/net10.0/ProjetoWeb01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff123587d80ea2486d2d1bd4efd771165fd9c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7f014f78d7a3cd4226be5950aa8da27a92b2b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWeb01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
